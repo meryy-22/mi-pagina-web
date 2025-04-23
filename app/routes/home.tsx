dcf28266085c9d0dcf7f37512fda55e0e208f7c2
Nocturno <nocturno@equipo-00>
2025-04-23 17:47:26 +0200

    pagina en react-router sin ssr

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,4 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-import { isFileServingAllowed } from "vite";
 
 export function meta({}: Route.MetaArgs) {
   return [
